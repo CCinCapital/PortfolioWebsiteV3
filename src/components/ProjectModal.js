67e6a220ed6e33bca4c96dd238801771e8c53f01
Can Chen <canchen@me.com>
2017-11-21 22:56:29 -0500

    init: init files

---
+++ src/components/ProjectModal.js
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react'
+
+class ProjectModal extends Component {
+
+  render () {
+    return (
+      <div>
+        <div className="logo-part one"></div>
+        <div className="logo-part two"></div>
+        <div className="logo-part three"></div>
+        <div className="logo-part four"></div>
+      </div>
+    )
+  }
+}
+
+export default ProjectModal
